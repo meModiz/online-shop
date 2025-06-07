241006b66f1776f17c37d70cdc7a7be288f55f77
Modestas A. <modestasanilionis2005@gmail.com>
2025-06-08 01:28:33 +0300

    chore(): remove debugging

--- backend/src/controllers/admin/getUsers.controller.ts
+++ backend/src/controllers/admin/getUsers.controller.ts
@@ -1,12 +1,8 @@
 import prisma from "../../prisma";
 import { Request, Response } from "express";
 import { HTTP } from "../../utils/statusCodes";
-import { getUserByJWT } from "../../services/jwt.services";
 
 export default async function GetUsers(req: Request, res: Response) {
   const users = await prisma.user.findMany();
-  const authenticated = await getUserByJWT(
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpasdasdXVCJ9.eyJleHAiOjE3NDkzMzE5MTYsImRhdGEiOnsidXNlcklkIjoxLCJ1c2VyRW1haWwiOiJtb2Rpc0BkYXVuYXMuY29tIn0sImlhdCI6MTc0OTMyODMxNn0.1YOT4V5E-SX_ZSEC70n4IJEsATMn8carq0KtZsb4eTI"
-  );
   res.status(HTTP.OK).json({ users: users });
 }
