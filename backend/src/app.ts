4e69a5e9fd63c49c583843bf15c72829f91ccf67
Modestas A. <modestasanilionis2005@gmail.com>
2025-06-12 01:12:44 +0300

    feat(): added cors, temp permission for fe until development

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import authRoutes from "./routes/auth.route";
 import adminRoutes from "./routes/admin.route";
 import productRoutes from "./routes/product.route";
@@ -6,7 +7,7 @@ import { limiter } from "./utils/api.limiter";
 import { checkAuth, isAdmin } from "./middlewares/auth.middleware";
 
 const app = express();
-
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json());
 app.use("/auth", limiter, authRoutes);
 
