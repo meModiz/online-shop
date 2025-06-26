5a1efbcda5204435294c163aff98a55bd5d52bd6
Modestas A. <modestasanilionis2005@gmail.com>
2025-06-26 21:53:23 +0300

    fix(fe); temp fix

--- front-end/src/app/admin/page.tsx
+++ front-end/src/app/admin/page.tsx
@@ -6,24 +6,21 @@ export default function AdminPage() {
   const router = useRouter();
 
   return (
-    <AdminPageRestriction
-      children={
-        <div className="flex flex-col justify-center items-center w-full px-40 pt-16 gap-5">
-          <span className="text-xl bg-black/80 rounded-xl p-4 text-white">
-            ADMIN MENU
-          </span>
-          <div className="flex gap-4 flex-col w-1/3 items-center justify-center">
-            <Button
-              title={"Users"}
-              onClick={() => router.push("admin/users")}
-            />
-            <Button
-              title={"Products"}
-              onClick={() => router.push("admin/products")}
-            />
-          </div>
-        </div>
-      }
-    />
+    // <AdminPageRestriction
+    //   children={
+    <div className="flex flex-col justify-center items-center w-full px-40 pt-16 gap-5">
+      <span className="text-xl bg-black/80 rounded-xl p-4 text-white">
+        ADMIN MENU
+      </span>
+      <div className="flex gap-4 flex-col w-1/3 items-center justify-center">
+        <Button title={"Users"} onClick={() => router.push("admin/users")} />
+        <Button
+          title={"Products"}
+          onClick={() => router.push("admin/products")}
+        />
+      </div>
+    </div>
+    //   }
+    // />
   );
 }
